82f8e4c2c8073bb55e5c305d3f6fed9fac3ef9c8
lihaodong <2409261729@qq.com>
2026-03-15 16:29:43 +0800

    reinforce策略梯度模型测试

---
+++ 策略梯度法/reinforce/test.py
@@ -0,0 +1,32 @@
+import gymnasium as gym
+import torch
+import torch.optim as optim
+
+from 策略梯度法.reinforce.reinforce import Agent
+
+
+#看测试效果
+def test_render(agent, env, episodes=5):
+    for episode in range(episodes):
+        state,_ = env.reset()
+        done = False
+        total_reward = 0
+
+        while not done:
+            env.render()
+            action, _ = agent.get_action(state)
+            next_state, reward, terminated,truncated,info = env.step(action)
+            done = terminated or truncated
+            state = next_state
+            total_reward += reward
+
+        print(f"回合 {episode + 1}: 总奖励 = {total_reward}")
+    env.close()
+
+
+# 加载模型后测试
+env = gym.make('CartPole-v1', render_mode='human')
+agent = Agent()
+agent.pi.load_state_dict(torch.load('models/policy_model.pth'))
+agent.pi.eval()
+test_render(agent, env)
